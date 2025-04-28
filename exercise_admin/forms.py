b4430ace26706a753a2a790c21e8b9bab7603a2a
didudua <126039750+didudua@users.noreply.github.com>
2025-04-28 15:09:29 +0700

    Delete english/migrations/0003_alter_payment_info_time_at_and_more.py

--- exercise_admin/forms.py
+++ exercise_admin/forms.py
@@ -15,16 +15,8 @@ class KhoaHocForm(forms.ModelForm):
 class BuoiHocForm(forms.ModelForm):
     class Meta:
         model = LESSON
-        fields = ['lesson_name', 'description', 'lesson_file', 'exercise_file']
+        fields = ['lesson_file', 'exercise_file']
         widgets = {
-            'lesson_name': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Nhập tên bài học'
-            }),
-            'description': forms.Textarea(attrs={
-                'class': 'form-control',
-                'placeholder': 'Nhập mô tả bài học'
-            }),
             'lesson_file': forms.ClearableFileInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Tải lên file bài học'
