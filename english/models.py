86d4e900969edc77f5e3081a1223913b546f39b7
tkieuvt <thanhkieu13052004@gmail.com>
2025-05-22 00:39:45 +0700

    update pro5 + registerCourse

--- english/models.py
+++ english/models.py
@@ -14,9 +14,9 @@ def submission_upload_path(instance, filename):
 class USER_PROFILE(models.Model):
     userprofile = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     SEX_CHOICES = [
-        ('M', 'Male'),
-        ('F', 'Female'),
-        ('O', 'Other'),
+        ('M', 'Nam'),
+        ('F', 'Nữ'),
+        ('O', 'Khác'),
     ]
     dob = models.DateField(default="")  # ngày sinh
     sex = models.CharField(max_length=1, choices=SEX_CHOICES,null=True)  # giới tính (có choices)
@@ -43,13 +43,6 @@ class QUESTION_MEDIA(models.Model):
     questionmedia_id = models.AutoField(primary_key=True)
     audio_file = models.FileField(upload_to='media/audio', null=True, blank=True)
     paragraph = models.TextField(null=True, blank=True)
-    # def __str__(self):
-    #     parts = []
-    #     if self.audio_file:
-    #         parts.append("🎧 Audio")
-    #     if self.paragraph:
-    #         parts.append("📖 Paragraph")
-    #     return " + ".join(parts) or "❓ Chưa có nội dung"
     class Meta:
         db_table = 'QUESTION_MEDIA'
 
