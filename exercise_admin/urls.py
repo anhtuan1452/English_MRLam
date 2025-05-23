2e2afda2697ac5cf9e46e16255ac7495d4084281
thaovi1406 <ngvi987654321@gmail.com>
2025-05-23 09:47:05 +0700

    part6

--- exercise_admin/urls.py
+++ exercise_admin/urls.py
@@ -4,8 +4,7 @@ from exercise_admin import views
 urlpatterns = [
     path('ql_baitap/', views.admin_ql_baitap, name='admin_ql_baitap'),
     path('ql_baitap/thembt/', views.them_baitap, name='them_baitap'),
-    path('ql_baitap/xembt/<int:lesson_id>/', views.xem_baitap, name='xem_baitap'),
-    path('ql_baitap/xembt/<int:lesson_id>/edit/', views.sua_baitap, name='sua_baitap'),
-    path('ql_baitap/xoa_baitap/<int:lesson_id>/', views.xoa_baitap, name='xoa_baitap'),
-    path('ql_baitap/xembt/<int:lesson_id>/sua/', views.sua_baitap_xem, name='sua_baitap_xem'),
+    path('ql_baitap/xembt/<int:lesson_detail_id>/', views.xem_baitap, name='xem_baitap'),
+    path('ql_baitap/xembt/<int:lesson_detail_id>/edit/', views.sua_baitap, name='sua_baitap'),
+    path('ql_baitap/xembt/<int:lesson_detail_id>/delete/', views.xoa_baitap, name='xoa_baitap'),
 ]
